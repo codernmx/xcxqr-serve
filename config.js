6bda5c1eb6484c7d7f69a45bc2194230315a5b65
codernmx <2507128400@qq.com>
2022-03-10 11:21:23 +0800

    init

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: '2002', // server port
+  template_id: '', // 模板消息ID
+  appId: 'wx285a242d191f9226', // 小程序appid
+  appSecret: '9a2df59bab8c6b744cf699749d5d6263', // 小程序secrect
+  env: 'codernmx-5gyxmux49f98c8b2' // 小程序云开发环境ID
+}
